98a4553aa8d91d9b756d00bdf0afec004f902006
johirul07 <johirulislam011022@gmail.com>
2025-12-13 22:45:59 +0600

    Remove node_modules and dist from repo

--- dist/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const logger = (req, res, next) => {
-    console.log(`[${new Date().toString()}] ${req.method} ${req.path}\n`);
-    next();
-};
-exports.logger = logger;
